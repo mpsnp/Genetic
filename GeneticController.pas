57c0c7b90e4c611a915b791be6c94e9665482225
mpsnp <kir_george@mail.ru>
2013-05-08 15:27:23 +0400

    Перевод на Lazarus

---
+++ GeneticController.pas
@@ -0,0 +1,26 @@
+unit GeneticController;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, Genetic;
+
+type
+  TForm1 = class(TForm)
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end; 
+
+var
+  Form1: TForm1; 
+
+implementation
+
+{$R *.lfm}
+
+end.
+
